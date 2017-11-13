1eb7f6a0061b7a39add3b3ae66be2b9fd502a57e
Simon Simonsson <simonsson.simon@gmail.com>
2017-11-13 14:53:06 +0100

    adding tests so they are available

---
+++ test/test.sh
@@ -0,0 +1,38 @@
+
+BIN=bin/nes
+TESTDIR="test/tests/"
+tests=($(find $TESTDIR -name "*.nes"))
+
+function print_tests {
+	i=0
+	for f in "${tests[@]}"
+	do
+		printf " [%.3d] %s\n" $i "$f"
+		i=$(( i + 1 ))
+	done
+	# print_files ${tests[@]}
+}
+
+echo "[ NES tests ]"
+echo "there are ${#tests[@]} tests"
+print_tests
+
+i=0
+while [[ 1 ]]
+do
+	echo -n ">> "
+	read i
+
+	case "$i" in
+		"quit" | "q")
+			break
+			;;
+	 	"list" | "l")
+			print_tests
+			;;
+		*)
+			echo "running ${tests[$i]}"
+			$BIN "${tests[$i]}"
+			;;
+	esac
+done
